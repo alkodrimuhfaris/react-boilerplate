c70718bb8e2e2778e18591f722014c71a09c23b0
alkodrimuhfaris <farisalkodri@gmail.com>
2021-09-25 20:29:00 +0700

    change use Window dimension

--- src/Helpers/useWindowDimension.js
+++ src/Helpers/useWindowDimension.js
@@ -6,10 +6,15 @@ function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
 
   const xs = width < 576;
-  const sm = width < 768;
-  const md = width < 992;
-  const lg = width < 1200;
+  const sm = width <= 767;
+  const md = width <= 991;
+  const lg = width <= 1199;
   const xl = width >= 1200;
+  const xsO = width <= 575;
+  const smO = width <= 767 && width >= 576;
+  const mdO = width <= 991 && width >= 768;
+  const lgO = width <= 1199 && width >= 992;
+  const xlO = width >= 1200;
 
   return {
     width,
@@ -19,13 +24,22 @@ function getWindowDimensions() {
     md,
     lg,
     xl,
+    xsO,
+    smO,
+    mdO,
+    lgO,
+    xlO,
   };
 }
 
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions(),
-  );
+  const [windowDimensions, setWindowDimensions] = useState({width: -1, height: -1,  xs : false, sm : false, md: false, lg: false, xl: false, xsO : false, smO : false, mdO: false, lgO: false, xlO: false});
+
+  useEffect(() => {
+    if (windowDimensions.width === -1) {
+      setWindowDimensions(getWindowDimensions());
+    }
+  }, []);
 
   useEffect(() => {
     function handleResize() {
